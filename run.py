3e8bdea12b61f9e2d16c9f2f4189dc5f8ef84748
jbocciadev <>
2023-05-23 18:38:22 +0000

    Initial commit with basic start

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os, time
+
+def clear():
+    """
+    Clears the terminal. See https://stackoverflow.com/questions/517970/how-to-clear-the-interpreter-console
+    """
+    os.system("clear")
+
+
+def run_game():
+    stolen = "The FIFA world cup trophy"
+    victim_location = "Buenos Aires, Argentina"
+
+    user = input(f"Identify yourself, agent!\nWhat is your name?\n")
+    clear()
+    print(f"Agent {user}, we have received a report from {victim_location} that {stolen} has been stolen.")
+    time.sleep(3)
+    print(f"You have 24 hours to catch the culprit.")
+    time.sleep(2)
+    print(f"Head over to the crime scene to begin your investigation, and good luck!")
+
+def main():
+    run_game()
+
+
+main()
